067e2632840e0715355921a637086a30f03c3a99
Lien Zhou <Lien.Zhou@shinda.com.tw>
2022-05-24 21:36:59 +0800

    在local Master修改5

--- test04/Program.cs
+++ test04/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("Hello, World3!");
 Console.WriteLine("Hello, World4!");Console.WriteLine("Hello, World2!");
 Console.WriteLine("Hello, 修改!");
 Console.WriteLine("Hello, 再修改!");
+Console.WriteLine("Hello, World5!");
